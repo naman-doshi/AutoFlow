3e85372b3fd65e2d56d839f21c09361bba065fbc
aerobull42 <doshinaman2006@gmail.com>
2023-09-15 12:19:38 +1000

    minor refactoring

--- randomGraph.py
+++ randomGraph.py
@@ -1,27 +1,37 @@
-from main import *
+from classdefs import *
 import random
 
-style = 'NY'
+style = 'New York'
 num_vertices = 50
 num_vehicles = 20
 width = 10
 height = 5
 prioritiseEV = False
 
-
-
-
-
-
 vertices = []
 roads = {}
 vehicles = []
+
+# Create vertices
 for i in range(num_vertices):
 	roads[i] = {}
 	vertices.append(Vertex(i))
 
-if style == "NY":
+# Create vehicles
+for i in range(num_vehicles):
+	start = random.randint(0, num_vertices - 1)
+	end = random.randint(0, num_vertices - 1)
+	while end == start:
+		end = random.randint(0, num_vertices - 1)
+	vehicle = Vehicle(vertices[start], vertices[end], random.randint(1, 3))
+	vehicles.append(vehicle)
 
+vehicles.sort(key = lambda x: x.emissions_per_hour, reverse = not prioritiseEV)
+
+# Construct graph according to city
+if style == "New York":
+
+	# New York — Grid structure
 	for i in range(num_vertices):
 		numLanes = random.randint(1, 3)
 		speedLimit = random.randint(20, 60)
@@ -33,16 +43,10 @@ if style == "NY":
 		if i < (height - 1) * width:
 			road = Road.connect(vertices[i], vertices[i + width], length, speedLimit, numLanes)
 			roads[i][i + width] = road
+
+
 	
-	for i in range(num_vehicles):
-		start = random.randint(0, num_vertices - 1)
-		end = random.randint(0, num_vertices - 1)
-		while end == start:
-			end = random.randint(0, num_vertices - 1)
-		vehicle = Vehicle(vertices[start], vertices[end], random.randint(1, 3))
-		vehicles.append(vehicle)
-	
-	vehicles.sort(key = lambda x: x.emissions_per_hour, reverse = not prioritiseEV)
+
 
 
 	
