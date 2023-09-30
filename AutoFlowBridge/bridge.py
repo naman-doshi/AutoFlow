900369669e4c07feb4f62c3ca0c663eedb5efd8c
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2023-09-30 22:19:26 +1000

    Bridge more messages

--- AutoFlowBridge/bridge.py
+++ AutoFlowBridge/bridge.py
@@ -1,5 +1,7 @@
 import asyncio
 import dataclasses
+import random
+from websockets.exceptions import ConnectionClosedOK
 from websockets.server import WebSocketServerProtocol, serve
 import json
 
@@ -7,11 +9,40 @@ PORT = 8001
 
 
 @dataclasses.dataclass
-class InitMessage:
-    matrix: list[list[str]]
+class VehicleInitMessage:
+    id: int
+    position: tuple[float, float]
+    rotation: float
 
     def __dict__(self):
-        return {"message": self.matrix, "type": "InitMessage"}
+        return {
+            "id": self.id,
+            "position": self.position,
+            "rotation": self.rotation,
+            "type": "VehicleInitMessage",
+        }
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return VehicleInitMessage(**d)
+
+
+@dataclasses.dataclass
+class InitMessage:
+    tiles: list[str]
+    rowWidth: int
+    vehicles: list[VehicleInitMessage]
+
+    def __dict__(self):
+        return {
+            "tiles": self.tiles,
+            "rowWidth": self.rowWidth,
+            "vehicles": [v.__dict__() for v in self.vehicles],
+            "type": "InitMessage",
+        }
 
     def serialize(self):
         return json.dumps(self.__dict__())
@@ -19,6 +50,46 @@ class InitMessage:
     @staticmethod
     def deserialize(d):
         return InitMessage(**d)
+
+
+@dataclasses.dataclass
+class VehicleUpdateMessage:
+    id: int
+    position: tuple[float, float]
+    relativeTime: float
+
+    def __dict__(self):
+        return {
+            "id": self.id,
+            "position": self.position,
+            "relativeTime": self.relativeTime,
+            "type": "VehicleUpdateMessage",
+        }
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return VehicleInitMessage(**d)
+
+
+@dataclasses.dataclass
+class UpdateMessage:
+    updates: list[VehicleUpdateMessage]
+
+    def __dict__(self):
+        return {
+            "updates": [u.__dict__() for u in self.updates],
+            "type": "UpdateMessage",
+        }
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return VehicleInitMessage(**d)
 
 
 class JSONUtils:
@@ -30,32 +101,43 @@ class JSONUtils:
         match typ:
             case "InitMessage":
                 return InitMessage.deserialize(d)
+            case "VehicleMessage":
+                return VehicleInitMessage.deserialize(d)
             case _:
                 raise ValueError("Invalid type from JSON")
 
 
 async def handler(websocket: WebSocketServerProtocol):
-    # Initialiize Unity Client
-    await websocket.send(InitMessage([["HR", "HR"], ["HR", "HR"]]).serialize())
+    print("Session opened")
+    await websocket.send(
+        InitMessage(
+            ["HR", "HR", "HR", "HR"], 2, [VehicleInitMessage(0, (0, 0), 0)]
+        ).serialize()
+    )
+
     while True:
-        await asyncio.sleep(1)
-
-    # while True:
-    #     msg = await websocket.recv()
-    #     initMessage = JSONUtils.des
-    #     # Receive
-    #     async for message in websocket:
-    #         print(message)
-
-    #     await asyncio.sleep(1)
+        try:
+            await websocket.send(
+                UpdateMessage(
+                    [
+                        VehicleUpdateMessage(
+                            0, (random.randrange(-20, 20), random.randrange(-20, 20)), 2
+                        )
+                    ]
+                ).serialize()
+            )
+            await asyncio.sleep(1)
+        except ConnectionClosedOK:
+            print("Session closed")
+            break
 
 
 async def main():
     async with serve(handler, "localhost", PORT):
-        try:
-            await asyncio.Future()  # run forever
-        except asyncio.CancelledError:
-            print("Terminated")
+        await asyncio.Future()  # run forever
 
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("Interrupted")
