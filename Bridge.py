50c2c8cfb1e426b39bd7a4cd03365fec58811b63
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2023-10-01 17:53:48 +1100

    Bridge

--- Bridge.py
+++ Bridge.py
@@ -5,6 +5,7 @@ from websockets.server import WebSocketServerProtocol, serve
 import json
 
 from AutoFlowBridgeCompat import outputToBridge
+from LandscapeComponents import Landscape
 
 PORT = 8001
 
@@ -32,28 +33,6 @@ class VehicleInitMessage:
 
 
 @dataclasses.dataclass
-class InitMessage:
-    tiles: list[str]
-    rowWidth: int
-    vehicles: list[VehicleInitMessage]
-
-    def __dict__(self):
-        return {
-            "tiles": self.tiles,
-            "rowWidth": self.rowWidth,
-            "vehicles": [v.__dict__() for v in self.vehicles],
-            "type": "InitMessage",
-        }
-
-    def serialize(self):
-        return json.dumps(self.__dict__())
-
-    @staticmethod
-    def deserialize(d):
-        return InitMessage(**d)
-
-
-@dataclasses.dataclass
 class Vector3Message:
     x: float
     y: float
@@ -67,13 +46,69 @@ class Vector3Message:
 
     @staticmethod
     def deserialize(d):
-        return VehicleInitMessage(**d)
+        return Vector3Message(**d)
+
+
+@dataclasses.dataclass
+class Vector2Message:
+    x: float
+    y: float
+
+    def __dict__(self):
+        return {"x": self.x, "y": self.y}
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return Vector2Message(**d)
+
+
+@dataclasses.dataclass
+class RoadInitMessage:
+    startPos: Vector2Message
+    endPos: Vector2Message
+
+    def __dict__(self):
+        return {"startPos": self.startPos.__dict__(), "endPos": self.endPos.__dict__()}
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return RoadInitMessage(**d)
+
+
+@dataclasses.dataclass
+class InitMessage:
+    tiles: list[str]
+    rowWidth: int
+    vehicles: list[VehicleInitMessage]
+    roads: list[RoadInitMessage]
+
+    def __dict__(self):
+        return {
+            "tiles": self.tiles,
+            "rowWidth": self.rowWidth,
+            "vehicles": [v.__dict__() for v in self.vehicles],
+            "roads": [r.__dict__() for r in self.roads],
+            "type": "InitMessage",
+        }
+
+    def serialize(self):
+        return json.dumps(self.__dict__())
+
+    @staticmethod
+    def deserialize(d):
+        return InitMessage(**d)
 
 
 @dataclasses.dataclass
 class VehicleUpdateMessage:
     id: int
-    route: list[Vector3Message]
+    route: list[Vector2Message]
 
     def __dict__(self):
         return {
@@ -126,31 +161,46 @@ class JSONUtils:
 async def handler(websocket: WebSocketServerProtocol):
     print("Session opened")
     inp: tuple[
-        dict[int, tuple[float, float]], dict[int, list[tuple[float, float, float]]]
+        dict[int, tuple[float, float]], Landscape, dict[int, list[tuple[float, float]]]
     ] = outputToBridge()
 
     # Initial scene
-    initMessages = []
+    vehicleInits = []
     for id, pos in inp[0].items():
-        initMessages.append(VehicleInitMessage(id, pos, 0))
+        vehicleInits.append(VehicleInitMessage(id, pos, 0))
+
+    flatLandscapeMatrix = []
+    for row in inp[1].landscapeMatrix:
+        flatLandscapeMatrix.extend(row)
+
+    roadMessages = []
+    for road in inp[1].roads:
+        roadMessages.append(
+            RoadInitMessage(
+                Vector2Message(road.startPosReal[0], road.startPosReal[1]),
+                Vector2Message(road.endPosReal[0], road.endPosReal[1]),
+            )
+        )
+
     await websocket.send(
-        InitMessage(["HR", "HR", "HR", "HR"], 2, initMessages).serialize()
+        InitMessage(
+            flatLandscapeMatrix,
+            len(inp[1].landscapeMatrix[0]),
+            vehicleInits,
+            roadMessages,
+        ).serialize()
     )
+    print("Finished terrain")
     await asyncio.sleep(0.5)
 
     # Updates
     updateMessages = []
-    for id, route in inp[1].items():
-        currentCar = VehicleUpdateMessage(id, [Vector3Message(*node) for node in route])
+    for id, route in inp[2].items():
+        currentCar = VehicleUpdateMessage(id, [Vector2Message(*r) for r in route])
         updateMessages.append(currentCar)
     await websocket.send(UpdateMessage(updateMessages).serialize())
 
-    while True:
-        try:
-            await asyncio.sleep(1)
-        except ConnectionClosedOK:
-            print("Session closed")
-            break
+    print("Finished routing")
 
 
 async def main():
