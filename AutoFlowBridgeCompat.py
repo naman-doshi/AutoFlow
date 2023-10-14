fb13ff43cfd668c7bdafa9d50a9df63a31f50cb0
James Jiang <jamesjiang2195@gmail.com>
2023-10-14 14:54:48 +1100

    Dummy

--- AutoFlowBridgeCompat.py
+++ AutoFlowBridgeCompat.py
@@ -160,7 +160,7 @@ print(len(available_destination_coordinates))
 print()
 print(MAX_VEHICLE_COUNT)
 print()
-#VEHICLE_COUNT = randint(int(MAX_VEHICLE_COUNT * 9 / 10), MAX_VEHICLE_COUNT) # auto-generated
+#VEHICLE_COUNT = randint(int(MAX_VEHICLE_COUNT * 9 / 10), MAX_VEHICLE_COUNT)
 VEHICLE_COUNT = randint(int(MAX_VEHICLE_COUNT * 7 / 10), int(MAX_VEHICLE_COUNT * 8 / 10)) # auto-generated
 
 # Check that the vehicle count does not exceed the maximum allowed vehicle count
