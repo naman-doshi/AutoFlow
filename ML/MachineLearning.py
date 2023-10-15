01d90984f5765d2bea109d7ab007d744231df78b
aerobull42 <doshinaman2006@gmail.com>
2023-10-15 19:35:52 +1100

    added ML

--- ML/MachineLearning.py
+++ ML/MachineLearning.py
@@ -1,16 +1,55 @@
+# Python
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the training data
-X_train = np.loadtxt('train_data.csv', delimiter=',')
-y_train = np.loadtxt('train_labels.csv', delimiter=',')
+data = pd.read_csv('train.csv')
 
-# Train the GBRT model
-model = GradientBoostingRegressor()
-model.fit(X_train, y_train)
+# Preprocessing
+# Assuming the last column is the target variable
+y = data.iloc[:, -1]
+X = data.iloc[:, :-1]
 
-# Sort a new array
-X_new = np.array([1, 2, 3])
-y_pred = model.predict(X_new)
+# Standardize the features
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
-# The predicted ranking is y_pred
+# Split the data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the model
+gbrt = GradientBoostingRegressor(random_state=42)
+
+# Define the grid of hyperparameters to search
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'learning_rate': [0.01, 0.1, 1],
+    'max_depth': [1, 2, 3]
+}
+
+# Setup the grid search
+grid_search = GridSearchCV(gbrt, param_grid, cv=5, scoring='neg_mean_squared_error')
+
+# Fit the model and find optimal hyperparameters
+grid_search.fit(X_train, y_train)
+
+# Get the best model
+best_gbrt = grid_search.best_estimator_
+
+# Predict on the validation set
+y_pred = best_gbrt.predict(X_val)
+
+# Sort the validation set based on the predictions
+sorted_indices = np.argsort(y_pred)
+X_val_sorted = X_val[sorted_indices]
+
+# Evaluate the model
+mse = mean_squared_error(y_val, y_pred)
+r2 = r2_score(y_val, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R^2 Score: {r2}')
