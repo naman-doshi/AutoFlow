c28863e42efc7406c2a3fb1295899b995ea9d36b
naman-doshi <doshinaman2006@gmail.com>
2023-10-27 09:33:56 +1100

    prioritise lower emission vehicles

--- AutoFlow.py
+++ AutoFlow.py
@@ -293,7 +293,7 @@ def sortVehicles(autoflow_vehicles: list[Vehicle]):
                 getRealPositionOnRoad(vehicle.road, vehicle.position),
                 getRealPositionOnRoad(vehicle.destinationRoad, vehicle.destinationPosition)
             ) * vehicle.passengerCount,
-            vehicle.emissionRate
+            vehicle.emissionRate * -1;
         ),
         reverse=True
     )
